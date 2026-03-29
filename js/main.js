5d2c9a56a0e065cd81f422f8b87c5cbe7a3d33a4
Collins <chukwuemekaigwelo@gmail.com>
2026-03-29 22:05:08 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// primitive datatype and Non-primitive datatypes  
+//PRIMITIVE DATATYPE
+
+// 1. Strings: Any data under single quote, double quote or backtick is a string.
+// 2.Numbers: intergers , big intergres and floats.
+// 3. Booleans: true or false value
+// 4. Null: empty value or no value
+// 5. Undefined:
+
+let lastname = Gain 
+let firstname = hum 
+console.log 
